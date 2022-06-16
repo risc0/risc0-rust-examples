b73a619a7f7559bb56aabb9e4ebae7248c0023a7
elakn <kalen.mills7@gmail.com>
2022-06-16 16:04:08 -0700

    adding license headers and removing file ID workaround from host

--- password-validity-checker/cli/src/main.rs
+++ password-validity-checker/cli/src/main.rs
@@ -1,4 +1,17 @@
-use rand::prelude::*;
+// Copyright 2022 Risc0, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 
 use std::fs;
 
@@ -7,7 +20,7 @@ use risc0_zkvm_core::Digest;
 use risc0_zkvm_host::Prover;
 use risc0_zkvm_serde::{from_slice, to_vec};
 use tempfile::tempdir;
-use tempfile::tempfile;
+use rand::prelude::*;
 
 fn main() {
     let password: &str = "S00perSecr1t!!!";
@@ -16,18 +29,6 @@ fn main() {
     let mut salt_bytes = [0u8, 32];
     rng.fill(&mut salt_bytes);
 
-    // Write the ID to a file.
-    // This is to work around the fact that the C++ prover API doesn't take IDs as buffers currently.
-    let temp_dir = tempdir().unwrap();
-    let id_path = temp_dir
-        .path()
-        .join("pw_checker.id")
-        .to_str()
-        .unwrap()
-        .to_string();
-    fs::write(id_path, PW_CHECKER_ID).unwrap();
-
-    // Using the file ID from the above workaround,
     // a new prover is created to run the pw_checker method
     let mut prover = Prover::new(&PW_CHECKER_PATH, PW_CHECKER_ID).unwrap();
 
@@ -40,6 +41,8 @@ fn main() {
         .unwrap();
 
     let receipt = prover.run().unwrap();
+    let password_hash: Digest = from_slice(&receipt.get_journal_vec().unwrap()).unwrap();
+    println!("Password hash is: {}", &password_hash);
 
     // In most scenarios, we would serialize and send the receipt to a verifier here
     // The verifier checks the receipt with the following call, which panics if the receipt is wrong

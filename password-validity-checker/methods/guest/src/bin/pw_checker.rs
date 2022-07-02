2aad8ce84adef08964983f3a807f277bae7f411f
Frank Laub <frank@risczero.com>
2022-07-01 19:54:30 -0700

    Update password checker & refactors

--- password-validity-checker/methods/guest/src/bin/pw_checker.rs
+++ password-validity-checker/methods/guest/src/bin/pw_checker.rs
@@ -13,102 +13,92 @@
 // limitations under the License.
 
 #![no_main]
-#![no_std]
 
+use password_validity_checker_core::PasswordRequest;
 use risc0_zkvm_guest::{env, sha};
 
 risc0_zkvm_guest::entry!(main);
 
-/* things we could do
-
-convert to Vec<u8> (import some stuff to help, look in the dev chat)
-convert str to String.as_bytes()
-*/
-
 pub fn main() {
-   let password: &str = env::read();
-   let salt_bytes: &[u8] = env::read();
+    let request: PasswordRequest = env::read();
 
-   let password_checker = PasswordValidityChecker{
-                                             min_length: 3,
-                                             max_length: 64,
-                                             min_numeric: 2,
-                                             min_uppercase: 2,
-                                             min_lowercase: 2,
-                                             min_special_chars: 1
-                                          };
+    let policy = PasswordPolicy {
+        min_length: 3,
+        max_length: 64,
+        min_numeric: 2,
+        min_uppercase: 2,
+        min_lowercase: 2,
+        min_special_chars: 1,
+    };
 
-   let valid_password: bool = password_checker.check_password_validity(&password);
-   if !valid_password {
+    if !policy.is_valid(&request.password) {
         panic!("Password invalid. Please try again.");
-   }
+    }
 
-   let mut password_byte_vec = password.as_bytes().to_vec();
-   let salted_password = password_byte_vec.extend(&salt_bytes.to_vec());
-   let password_hash = sha::digest(&salted_password);
+    let mut salted_password = request.password.as_bytes().to_vec();
+    salted_password.extend(request.salt);
+    let password_hash = sha::digest_u8_slice(&salted_password[..]);
+
     env::commit(&password_hash);
-    env::commit(&salt_bytes);
+    env::commit(&request.salt);
 }
 
-pub struct PasswordValidityChecker {
-   pub min_length: usize,
-   pub max_length: usize,
-   pub min_uppercase: usize,
-   pub min_lowercase: usize,
-   pub min_numeric: usize,
-   pub min_special_chars: usize
+struct PasswordPolicy {
+    pub min_length: usize,
+    pub max_length: usize,
+    pub min_uppercase: usize,
+    pub min_lowercase: usize,
+    pub min_numeric: usize,
+    pub min_special_chars: usize,
 }
 
-impl PasswordValidityChecker {
-   pub fn check_password_validity(&self, pw: &str) -> bool {
-      self.correct_length(pw)
-      && (self.count_numeric(pw) > self.min_numeric - 1)
-      && (self.count_uppercase(pw) > self.min_uppercase - 1)
-      && (self.count_lowercase(pw) > self.min_lowercase - 1)
-      && (self.count_special_chars(pw) > self.min_special_chars - 1)
-   }
-   
-   fn correct_length(&self, password: &str) -> bool {
-      password.len() > (self.min_length - 1) && password.len() < (self.max_length + 1)
-   }
+impl PasswordPolicy {
+    pub fn is_valid(&self, pw: &str) -> bool {
+        let metrics = PasswordMetrics::new(pw);
+        self.correct_length(pw)
+            && (metrics.numeric >= self.min_numeric)
+            && (metrics.uppercase >= self.min_uppercase)
+            && (metrics.lowercase >= self.min_lowercase)
+            && (metrics.special >= self.min_special_chars)
+    }
 
-   fn count_numeric(&self, password: &str) -> usize {
-      let mut numeric_char_count: usize = 0;
-      for c in password.chars() {
-         if c.is_ascii_digit() {
-            numeric_char_count += 1;
-         }
-      }
-      numeric_char_count
-   }
+    fn correct_length(&self, password: &str) -> bool {
+        password.len() > (self.min_length - 1) && password.len() < (self.max_length + 1)
+    }
+}
 
-   fn count_special_chars(&self, password: &str) -> usize {
-      let mut special_char_count: usize = 0;
-      for c in password.chars() {
-         if c.is_ascii_punctuation() {
-            special_char_count += 1;
-         }
-      }
-      special_char_count
-   }
+struct PasswordMetrics {
+    pub numeric: usize,
+    pub special: usize,
+    pub uppercase: usize,
+    pub lowercase: usize,
+}
 
-   fn count_uppercase(&self, password: &str) -> usize {
-      let mut caps_char_count: usize = 0;
-      for c in password.chars() {
-         if c.is_ascii_uppercase() {
-            caps_char_count += 1;
-         }
-      }
-      caps_char_count
-   }
-
-   fn count_lowercase(&self, password: &str) -> usize {
-      let mut lower_char_count: usize = 0;
-      for c in password.chars() {
-         if c.is_ascii_lowercase() {
-            lower_char_count += 1;
-         }
-      }
-      lower_char_count
-   }
+impl PasswordMetrics {
+    pub fn new(password: &str) -> Self {
+        let mut numeric = 0;
+        let mut special = 0;
+        let mut uppercase = 0;
+        let mut lowercase = 0;
+        for ch in password.chars() {
+            if ch.is_ascii_digit() {
+                numeric += 1;
+            }
+            if ch.is_ascii_punctuation() {
+                special += 1;
+            }
+            if ch.is_ascii_uppercase() {
+                uppercase += 1;
+            }
+            if ch.is_ascii_lowercase() {
+                lowercase += 1;
+            }
+        }
+        PasswordMetrics {
+            numeric,
+            special,
+            uppercase,
+            lowercase,
+        }
+    }
 }

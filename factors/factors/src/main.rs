935e8b38e4dda07277244c7913b64871e05ac960
Tim Zerrell <tim.zerrell@risczero.com>
2022-10-11 15:05:18 -0700

    Add factors example (#13)
    * Add factors example
    
    * Add running instructions to factor example
    
    * Retain existing note about rustup
    
    * Replace unwraps with expects
    
    * Lint

---
+++ factors/factors/src/main.rs
@@ -0,0 +1,42 @@
+use methods::{MULTIPLY_ID, MULTIPLY_PATH};
+use risc0_zkvm::host::Prover;
+use risc0_zkvm::serde::{from_slice, to_vec};
+
+fn main() {
+    // Pick two numbers
+    let a: u64 = 17;
+    let b: u64 = 23;
+
+    // Multiply them inside the ZKP
+    // First, we make the prover, loading the 'multiply' method
+    let multiply_src = std::fs::read(MULTIPLY_PATH)
+        .expect("Method code should be present at the specified path; did you use the correct *_PATH constant?");
+    let mut prover = Prover::new(&multiply_src, MULTIPLY_ID).expect(
+        "Prover should be constructed from valid method source code and corresponding method ID",
+    );
+
+    // Next we send a & b to the guest
+    prover.add_input(to_vec(&a).unwrap().as_slice()).unwrap();
+    prover.add_input(to_vec(&b).unwrap().as_slice()).unwrap();
+    // Run prover & generate receipt
+    let receipt = prover.run()
+        .expect("Valid code should be provable if it doesn't overflow the cycle limit. See `embed_methods_with_options` for information on adjusting maximum cycle count.");
+
+    // Extract journal of receipt (i.e. output c, where c = a * b)
+    let c: u64 = from_slice(
+        &receipt
+            .get_journal_vec()
+            .expect("Journal should be available for valid receipts"),
+    )
+    .expect("Journal output should deserialize into the same types (& order) that it was written");
+
+    // Print an assertion
+    println!("I know the factors of {}, and I can prove it!", c);
+
+    // Here is where one would send 'receipt' over the network...
+
+    // Verify receipt, panic if it's wrong
+    receipt.verify(MULTIPLY_ID).expect(
+        "Code you have proven should successfully verify; did you specify the correct method ID?",
+    );
+}

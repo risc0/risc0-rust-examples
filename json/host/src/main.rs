2c24113ea37fab0de8485dac3ccefd5fdd5d2884
Tim Zerrell <tim.zerrell@risczero.com>
2022-11-28 10:10:48 -0800

    Clean up receipt parsing

--- json/host/src/main.rs
+++ json/host/src/main.rs
@@ -1,6 +1,7 @@
 use std::io::prelude::*;
 
 use methods::{SEARCH_JSON_ID, SEARCH_JSON_PATH};
+use risc0_zkp::core::sha::Digest;
 use risc0_zkvm::host::Prover;
 use risc0_zkvm::serde::{from_slice, to_vec};
 
@@ -25,22 +26,9 @@ fn main() {
     receipt.verify(SEARCH_JSON_ID)
         .expect("Code you have proven should successfully verify; did you specify the correct method ID?");
 
-    let val: u32 = from_slice(
-        &receipt
-            .get_journal_vec()
-            .expect("Journal should be available for valid receipts"),
-    )
-    .expect("Journal output should deserialize into the same types (& order) that it was written");
+    let journal = &receipt.get_journal_vec().expect("Receipt should have journal");
+    let val: u32 = journal[0];
+    let digest = from_slice::<Digest>(&journal[1..]).expect("Journal should contain SHA Digest");
 
-    let mut first_run: bool = true;
-    let mut sha_str = String::from("");
-    for entry in &receipt.get_journal_vec().expect("WIP") {
-        if first_run {
-            first_run = false;
-        } else {
-            sha_str += &format!("{entry:x}")[..];
-        }
-    }
-
-    println!("\nThe JSON file with hash\n  {}\nprovably contains a field 'critical_data' with value {}\n", sha_str, val);
+    println!("\nThe JSON file with hash\n  {}\nprovably contains a field 'critical_data' with value {}\n", digest, val);
 }

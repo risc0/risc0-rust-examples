6f66fa265135a62bb6a8b3edac8fe6a0b616e5b1
Tim Zerrell <tim.zerrell@risczero.com>
2022-12-03 21:40:21 -0800

    Format

--- chess/host/src/main.rs
+++ chess/host/src/main.rs
@@ -1,10 +1,10 @@
+use chess_core::Inputs;
 use clap::{Arg, Command};
 use methods::{CHECKMATE_ID, CHECKMATE_PATH};
 use risc0_zkvm::host::Prover;
 use risc0_zkvm::serde::{from_slice, to_vec};
-use shakmaty::{fen::Fen, CastlingMode, Chess, FromSetup, Position, Setup};
-
-use chess_core::Inputs;
+use shakmaty::fen::Fen;
+use shakmaty::{CastlingMode, Chess, FromSetup, Position, Setup};
 
 fn pad_to_word(inp: &mut String) {
     inp.push_str(&"     "[..4 - (inp.len() % 4)]);
@@ -31,11 +31,12 @@ fn main() {
     let method_code = std::fs::read(CHECKMATE_PATH).unwrap();
     let mut prover = Prover::new(&method_code, CHECKMATE_ID).unwrap();
 
-    prover
-        .add_input(&to_vec(&inputs).unwrap()).unwrap();
+    prover.add_input(&to_vec(&inputs).unwrap()).unwrap();
 
     // Run prover & generate receipt
-    let receipt = prover.run().expect("Legal board state and checkmating move expected");
+    let receipt = prover
+        .run()
+        .expect("Legal board state and checkmating move expected");
 
     // Verify receipt and parse it for committed data
     receipt.verify(CHECKMATE_ID).unwrap();

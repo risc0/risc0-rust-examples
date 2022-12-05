7cefc9d940373fb3a4a327d04a1b19614d259855
Tim Zerrell <tim.zerrell@risczero.com>
2022-12-05 11:36:45 -0800

    Add chess example (#19)
    * Add chess example
    
    * Expand readme
    
    * Simplify expects to unwraps
    
    * Commit chess .lock files
    
    * Add chess to CI
    
    * Format

---
+++ chess/methods/guest/src/bin/checkmate.rs
@@ -0,0 +1,21 @@
+#![no_main]
+
+use shakmaty::{Chess, Position, fen::Fen, Setup, CastlingMode, FromSetup, Move, san::San};
+use chess_core::Inputs;
+use risc0_zkvm_guest::env;
+
+risc0_zkvm_guest::entry!(main);
+
+pub fn main() {
+    let inputs: Inputs = env::read();
+    let mv: String = inputs.mv;
+    let initial_state: String = inputs.board;
+    env::commit(&initial_state);
+
+    let setup = Setup::from(Fen::from_ascii(initial_state.as_bytes()).unwrap());
+    let pos = Chess::from_setup(setup, CastlingMode::Standard).unwrap();
+
+    let mv: Move = mv.parse::<San>().unwrap().to_move(&pos).unwrap();
+    let pos = pos.play(&mv).unwrap();
+    assert!(pos.is_checkmate());
+}

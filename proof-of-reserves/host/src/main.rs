d0d2cda35d9d24695ced8f8a61313469b97419fa
Joby Thundil <joby@risczero.com>
2022-12-15 17:27:56 -0600

    fix cargo.toml

--- proof-of-reserves/host/src/main.rs
+++ proof-of-reserves/host/src/main.rs
@@ -1,6 +1,9 @@
 // TODO: Update the name of the method loaded by the prover. E.g., if the method is `multiply`, replace `METHOD_NAME_ID` with `MULTIPLY_ID` and replace `METHOD_NAME_PATH` with `MULTIPLY_PATH`
+use bitcoin::hashes::hex::FromHex;
 use bitcoin::util::address::Address;
+use bitcoin::{MerkleBlock, Txid};
 use methods::{METHOD_NAME_ID, METHOD_NAME_PATH};
+use reqwest::blocking::Client;
 use risc0_zkvm::Prover;
 // use risc0_zkvm::serde::{from_slice, to_vec};
 use secp256k1::{PublicKey, Secp256k1, SecretKey};
@@ -42,6 +45,18 @@ fn private_key_to_address(private_key: String) -> Address {
     )
 }
 
+fn verify_merkle_proof(expected_tx_hash: String, expected_tx_index: u32, merkle_proof: String) {
+    let mb: MerkleBlock =
+        bitcoin::consensus::deserialize(&Vec::from_hex(&merkle_proof).unwrap()).unwrap();
+    let mut matches: Vec<Txid> = vec![];
+    let mut index: Vec<u32> = vec![];
+    assert!(mb.extract_matches(&mut matches, &mut index).is_ok());
+    assert_eq!(1, matches.len());
+    assert_eq!(Txid::from_hex(&expected_tx_hash).unwrap(), matches[0]);
+    assert_eq!(1, index.len());
+    assert_eq!(expected_tx_index, index[0]);
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -52,5 +67,16 @@ mod tests {
             "18E14A7B6A307F426A94F8114701E7C8E774E7F9A47E2C2035DB29A206321725".to_string();
         let address = private_key_to_address(private_key);
         assert_eq!(address.to_string(), "1PMycacnJaSqwwJqjawXBErnLsZ7RkXUAs");
+    }
+
+    #[test]
+    fn verify_merkle_proof_works() {
+        let proof =
+            "01000000ba8b9cda965dd8e536670f9ddec10e53aab14b20bacad27b9137190000000000190760b278fe7b8565fda3b968b918d5fd997f993b23674c0af3b6fde300b38f33a5914ce6ed5b1b01e32f570200000002252bf9d75c4f481ebb6278d708257d1f12beb6dd30301d26c623f789b2ba6fc0e2d32adb5f8ca820731dff234a84e78ec30bce4ec69dbd562d0b2b8266bf4e5a0105".to_string();
+        let expected_tx_hash =
+            "5a4ebf66822b0b2d56bd9dc64ece0bc38ee7844a23ff1d7320a88c5fdb2ad3e2".to_string();
+        let expected_tx_index = 1;
+
+        verify_merkle_proof(expected_tx_hash, expected_tx_index, proof)
     }
 }

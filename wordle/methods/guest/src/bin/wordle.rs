4e0ece8ba6624ada741020c7393af517d1e8f8e8
Jacob Weightman <jacobdweightman@gmail.com>
2023-01-09 18:04:19 -0600

    Add length checks to Wordle (#38)

--- wordle/methods/guest/src/bin/wordle.rs
+++ wordle/methods/guest/src/bin/wordle.rs
@@ -1,7 +1,7 @@
 #![no_main]
 
 use risc0_zkvm_guest::{env, sha};
-use wordle_core::{WordFeedback, LetterFeedback, GameState};
+use wordle_core::{WORD_LENGTH, WordFeedback, LetterFeedback, GameState};
 
 risc0_zkvm_guest::entry!(main);
 
@@ -9,11 +9,19 @@ pub fn main() {
     let word: String = env::read();
     let guess: String = env::read();
 
+    if word.chars().count() != WORD_LENGTH {
+        panic!("secret word must have length 5!")
+    }
+
+    if guess.chars().count() != WORD_LENGTH {
+        panic!("guess must have length 5!")
+    }
+
     let correct_word_hash = sha::digest_u8_slice(&word.as_bytes()).to_owned();
     env::commit(&correct_word_hash);
 
     let mut score: WordFeedback = WordFeedback::default();
-    for i in 0..5 {
+    for i in 0..WORD_LENGTH {
         score[i] = if word.as_bytes()[i] == guess.as_bytes()[i] {
             LetterFeedback::LetterCorrect
         } else if word.as_bytes().contains(&guess.as_bytes()[i]) {

01773daf388c437fb7eaf059d4d07bc5542935b4
Tim Zerrell <tim.zerrell@risczero.com>
2023-01-26 15:42:11 -0800

    Update voting-machine

--- voting-machine/methods/guest/src/bin/init.rs
+++ voting-machine/methods/guest/src/bin/init.rs
@@ -15,11 +15,11 @@
 #![no_main]
 #![no_std]
 
-use risc0_zkvm_guest::{env, sha};
+use risc0_zkvm::guest::{env, sha};
 
 use voting_machine_core::{InitializeVotingMachineCommit, VotingMachineState};
 
-risc0_zkvm_guest::entry!(main);
+risc0_zkvm::guest::entry!(main);
 
 pub fn main() {
     let state: VotingMachineState = env::read();
